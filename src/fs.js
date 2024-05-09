ac4cf895ce40059365db8b8538d9d2ce38f9670b
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-05-09 23:13:26 +0530

    update work

--- src/fs.js
+++ src/fs.js
@@ -3,53 +3,45 @@ const fs = require('fs');
 // The fs module in Node.js provides a way to interact with the file system on your computer. It allows you to perform various operations like reading, writing, creating, updating, deleting files and directories. It's a core module, which means you don't need to install it separately; it's built into Node.js.
 
 
-// 1. Reading a file
-fs.readFile('example.txt', 'utf8', (err, data) => {
+// reading file
+fs.readFile('file.txt', 'utf8', (err, data) => {
     if (err) {
-        console.error('Error reading file:', err);
+        console.error(err);
         return;
     }
-    console.log('File content:', data);
+    console.log(data);
 });
 
-
-// 2. Writing to a file
-const content = 'Hello, Node.js!';
-
-fs.writeFile('example.txt', content, 'utf8', (err) => {
+// writing file
+fs.writeFile('file.txt', 'Hello, im node!', (err) => {
     if (err) {
-        console.error('Error writing file:', err);
+        console.error(err);
         return;
     }
-    console.log('File written successfully!');
+    console.log('File has been written.');
 });
 
+// checking file existence
+if (fs.existsSync('file.txt')) {
+    console.log('File exists.');
+} else {
+    console.log('File does not exist.');
+}
 
-// 3. Creating a directory
-fs.mkdir('documents', (err) => {
+
+// Create new file
+fs.mkdir('greatDir', (err) => {
     if (err) {
-        console.error('Error creating directory:', err);
+        console.error(err);
         return;
     }
-    console.log('Directory created successfully!');
+    console.log('Directory created successfully.');
 });
 
-
-// 4. Renaming a file
-fs.rename('example.txt', 'new-example.txt', (err) => {
-    if (err) {
-        console.error('Error renaming file:', err);
-        return;
-    }
-    console.log('File renamed successfully!');
-})
-
-
-// 5. Deleting a file
-fs.unlink('example.txt', (err) => {
-    if (err) {
-        console.error('Error deleting file:', err);
-        return;
-    }
-    console.log('File deleted successfully!');
-});
+// fs.unlink('new.txt', (err) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log('File deleted successfully.');
+// });
