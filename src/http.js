b34c8a8bedba762794d50cf85cebb19366e02c14
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-05-12 20:59:12 +0530

    Date_10-5-24_Done_Homework

--- src/http.js
+++ src/http.js
@@ -2,14 +2,22 @@ const http = require('http')
 const url = require('url')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    console.log(req.method);
-    console.log(url.parse(req.url, true).query);
+    // console.log(req.url)
+    // console.log(req.method);
+    // console.log(url.parse(req.url, true).query);
 
-    res.writeHead(200, {'Content-Type': 'text.html'})
+    res.writeHead(200, { 'Content-Type': 'text.html' })
     res.write('hello world')
     res.end('end of the World')
 })
+
+fs.readFile('file.txt', "utf-8", function (err, data) {
+    res.write(data);
+    console.log(data);
+    res.end()
+
+});
+
 
 server.listen(3000, () => {
     // console.log('server is running on port 3000')
