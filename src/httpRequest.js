0446afa2b89ba977fde799a9784f958513f15fcb
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-05-14 09:27:25 +0530

    Date_14-5-24_Done_Classwork

--- src/httpRequest.js
+++ src/httpRequest.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
         fs.readFile('src/data/data.json', 'utf8', (err, data) => {
             if (err) {
                 res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify("Internal Server Error"));
+                res.end(JSON.stringify({ errorMessage: "Internal Server Error" }));
                 console.error(err);
                 return;
             }
@@ -25,14 +25,109 @@ const server = http.createServer((req, res) => {
                     res.end(JSON.stringify(obj));
                 } else {
                     res.writeHead(404, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify("data not found" ));
+                    res.end(JSON.stringify({ errorMessage: "Data not found" }));
                 }
             } else {
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(data);
             }
         });
+    } else if (method === 'post' && path === '/institute') {
+        let body = '';
+
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            try {
+                const newData = JSON.parse(body);
+                const alldata = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
+                alldata.push(newData);
+
+                fs.writeFile('src/data/data.json', JSON.stringify(alldata), (err) => {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ errorMessage: "Internal Server Error" }));
+                        console.error(err);
+                        return;
+                    }
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ successMessage: 'Data Added Successfully' }));
+                });
+            } catch (error) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ errorMessage: "Bad Request" }));
+            }
+        });
+    } else if (method === 'delete' && path === '/institute') {
+        try {
+            const id = url.parse(req.url, true).query.id;
+            const alldata = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
+            const data = alldata.filter((v) => v.id != id);
+
+            fs.writeFile('src/data/data.json', JSON.stringify(data), (err) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ errorMessage: "Internal Server Error" }));
+                    console.error(err);
+                    return;
+                }
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ successMessage: 'Data Deleted Successfully' }));
+            });
+        } catch (error) {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ errorMessage: "Bad Request" }));
+        }
+    } else if (method === 'put' && path === '/institute') {
+        let body = '';
+    
+        req.on('data', (chunk) => {
+            body += chunk.toString();
+        });
+    
+        req.on('end', () => {
+            try {
+                const newData = JSON.parse(body);
+    
+                fs.readFile('src/data/data.json', 'utf-8', (err, data) => {
+                    if (err) {
+                        res.writeHead(500, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ errorMessage: "Internal Server Error" }));
+                        console.error(err);
+                        return;
+                    }
+    
+                    const alldata = JSON.parse(data);
+                    const index = alldata.findIndex((v) => v.id == newData.id);
+    
+                    if (index !== -1) {
+                        alldata[index] = newData;
+    
+                        fs.writeFile('src/data/data.json', JSON.stringify(alldata), (err) => {
+                            if (err) {
+                                res.writeHead(500, { 'Content-Type': 'application/json' });
+                                res.end(JSON.stringify({ errorMessage: "Internal Server Error" }));
+                                console.error(err);
+                                return;
+                            }
+    
+                            res.writeHead(200, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ successMessage: 'Data Updated Successfully' }));
+                        });
+                    } else {
+                        res.writeHead(404, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ errorMessage: "Data not found" }));
+                    }
+                });
+            } catch (error) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ errorMessage: "Bad Request" }));
+            }
+        });
     }
+    
 });
 
 server.listen(3000, () => {
